29062d6e65d1eb628153798d9848bd17e5ed58d0
lixiaorong <lixiaorong@novelpro.cn>
2021-06-30 14:20:51 +0800

    x

--- install/bsw.sh
+++ install/bsw.sh
@@ -45,6 +45,10 @@ do
             top="${2}"
             shift 2
             ;;
+        --database-dsn)
+            databaseDsn="${2}"
+            shift 2
+            ;;
         --database)
             database="${2}"
             shift 2
@@ -112,6 +116,7 @@ if [[ "${help}" == "yes" ]]; then
     color 32 "--www"            "\t"  "\t\t\t[ ] 三级域名名称 [default:app]" "" "\n"
     color 32 "--host"           "\t"  "\t\t\t[ ] 二级域名名称 [default:{args.app}]" "" "\n"
     color 32 "--top"            "\t"  "\t\t\t[ ] 顶级域名名称 [default:com]" "" "\n"
+    color 32 "--database-dsn"   "\t"  "\t\t[ ] 数据库连接，优先级高于 --database 参数，格式：用户名:密码@主机:端口/数据库名，root:123456@host:3306/game"
     color 32 "--database"       "\t"  "\t\t[ ] 数据库名称 [default:{args.app}]"
     color 32 "--ir-admin-phone" "\t"  "\t[ ] 后台管理系统账号 [default:17011223344]" "" "\n\n"
     color 32 "--ir-admin-pwd"   "\t"  "\t\t[ ] 后台管理系统账号密码 [default:bsw@2020#BSW]" "" "\n"
@@ -207,7 +212,12 @@ if [[ "${mode}" == "create" ]]; then
         sed -i "s/bsw-host/${host}/g" ${item}
         sed -i "s/bsw-app/${app}/g" ${item}
         sed -i "s/bsw-type/${type}/g" ${item}
-        sed -i "s/bsw-database/${database}/g" ${item}
+        if [[ -n "${databaseDsn}" == "" ]]; then
+            sed -i "s/root\:Mys9l\!7259\@mysql\:3306\/bsw-database/${databaseDsn}/g" ${item}
+        else
+            sed -i "s/bsw-database/${database}/g" ${item}
+        fi
+
         color 37 "${item}" "\t" ""
 
         for p in 3306 6379 80 443;
