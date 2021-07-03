eca6c02a7853dfbd82eeb449386e56a0d04d93b1
lixiaorong <lixiaorong@novelpro.cn>
2021-07-03 20:16:56 +0800

    结构调整

--- public/dist/js/app.js
+++ public/dist/js/app.js
@@ -1,3 +1,3 @@
 /*! Anyone */
-/*! CustomerWeb - v0.0.1 - 2021-06-30 */
+/*! CustomerWeb - v0.0.1 - 2021-07-03 */
 "use strict";bsw.configure({data:{},method:{},logic:{loginAnimation:function(a){a.$nextTick(function(){bsw.doAnimateCSS(".login-box > div.left","flip","1.2s")})}}});
