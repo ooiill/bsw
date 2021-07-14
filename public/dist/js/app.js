7b1dc72c465f0ad5ad86558782047b247b14cb37
lixiaorong <lixiaorong@novelpro.cn>
2021-07-14 21:55:42 +0800

    css

--- public/dist/js/app.js
+++
@@ -1,3 +0,0 @@
-/*! Anyone */
-/*! CustomerWeb - v0.0.1 - 2021-07-10 */
-"use strict";bsw.configure({data:{},method:{},logic:{loginAnimation:function(a){a.$nextTick(function(){bsw.doAnimateCSS(".login-box > div.left","flip","1.2s")})}}});
--- src/Controller/MixedWebController.php
+++ src/Controller/MixedWebController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Leon\BswBundle\Annotation\Entity\Input as I;
 use Leon\BswBundle\Annotation\Entity\Output as O;
+use Leon\BswBundle\Module\Entity\Abs;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Leon\BswBundle\Controller\BswMixed\Upload;
 
@@ -25,5 +27,44 @@ class MixedWebController extends AcmeWebController
     public function uploadOptionsHandler(string $flag, array $options): array
     {
         return $options;
+    }
+
+    /**
+     * Document bsw
+     *
+     * @Route("/skills/{name}", name="app_skills_document", requirements={"name": "[a-zA-Z0-9\-\.]+"}, defaults={"name": "01.docker"})
+     *
+     * @param string $name
+     *
+     * @return Response
+     * @throws
+     */
+    public function document(string $name): Response
+    {
+        if (($args = $this->valid(Abs::V_NOTHING)) instanceof Response) {
+            return $args;
+        }
+
+        $this->appendSrcCssWithKey('markdown', Abs::CSS_MARKDOWN);
+        $this->appendSrcCssWithKey('highlight', Abs::CSS_HIGHLIGHT);
+        $this->appendSrcCssWithKey('scroll', 'diy:scroll');
+        $this->appendSrcJsWithKey('highlight', Abs::JS_HIGHLIGHT);
+
+        $this->seoWithAppName = false;
+        $markdown = $this->markdownDirectoryParse(
+            $name,
+            $this->getPath('mixed/skills', false),
+        );
+
+        // $this->cnf->font_symbol = null; // Not load iconfont.js
+
+        return $this->showPage(
+            [
+                'args' => compact('name'),
+                'data' => ['data' => $markdown],
+            ],
+            [],
+            'layout/document.html'
+        );
     }
 }
